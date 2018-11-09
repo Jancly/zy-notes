b80032447410a3c531a32ece003bde4b23cd4253
Janefane <1083146266@qq.com>
2018-11-09 12:50:38 +0800

    s

---
+++ notes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/deepin/github/zy-notes
+
+git pull origin master
+
+git add --all
+
+git commit -m “zy notes”
+
+git push origin master
---
+++ wifi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/deepin/software/create_ap
+
+sudo create_ap wlp2s0 ppp0 chen zong123456
